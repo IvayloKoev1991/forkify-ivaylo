6914e399cc899fc88abaf2e3c0d62094295a2157
IvayloKoev1991 <ivaylokoev1991@gmail.com>
2022-09-20 15:36:19 +0300

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -118,9 +118,6 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(err.message);
   }
 };
-const newFeature = function () {
-  console.log(`Welcome to the application!`);
-};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -129,6 +126,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
