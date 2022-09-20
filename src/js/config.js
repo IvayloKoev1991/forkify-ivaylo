241c033fc570792818c40c9735cd4d4dbcbe53d1
IvayloKoev1991 <ivaylokoev1991@gmail.com>
2022-09-20 14:55:14 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2d851999-4045-401c-87ee-97de7a525a5e';
+export const MODAL_CLOSE_SEC = 2.5;
